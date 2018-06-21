c54a27f68e5842e5e083d01566e99777e055eed3
maxinakenty <mgreen097@gmail.com>
2018-06-21 21:35:27 +0300

    add docs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import io from 'socket.io-client';
+import Controller from './js/Controller';
 import './styles/main.css';
+
+const socket = io();
+const carElem = document.getElementById('car');
+const controller = new Controller();
+controller.init();
+
+socket.on('drive a car', direction => {
+  const carStyle = getComputedStyle(carElem);
+
+  if (direction === 'top') {
+    carElem.style.top = `${parseFloat(carStyle.top) - 30}px`;
+  } else if (direction === 'left') {
+    carElem.style.left = `${parseFloat(carStyle.left) - 30}px`;
+  } else if (direction === 'bottom') {
+    carElem.style.top = `${parseFloat(carStyle.top) + 30}px`;
+  } else if (direction === 'right') {
+    carElem.style.left = `${parseFloat(carStyle.left) + 30}px`;
+  }
+
+  return false;
+});
---
+++ src/js/Controller.js
@@ -0,0 +1,37 @@
+import io from 'socket.io-client';
+
+const socket = io();
+
+class Controller {
+  static handleKeyDown = () => {
+    document.body.addEventListener(
+      'keydown',
+      e => {
+        const code = e.keyCode;
+
+        if (code === 37) {
+          socket.emit('drive a car', 'left');
+        }
+
+        if (code === 38) {
+          socket.emit('drive a car', 'top');
+        }
+
+        if (code === 39) {
+          socket.emit('drive a car', 'right');
+        }
+
+        if (code === 40) {
+          socket.emit('drive a car', 'bottom');
+        }
+      },
+      false,
+    );
+  };
+
+  init() {
+    this.constructor.handleKeyDown();
+  }
+}
+
+export default Controller;
